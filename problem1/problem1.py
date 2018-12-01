af925d441f645cdcd28b8cab0955f1f4960a0d52
明俊 <daniel-simon-gonzalez@protonmail.ch>
2018-12-01 01:55:35 -0500

    Changes to be committed: 	modified:   problem1/problem1.py

--- problem1/problem1.py
+++ problem1/problem1.py
@@ -1,8 +1,11 @@
+# dan 明俊
+
 from __future__ import print_function
 import numpy as np
 
 with open("./input") as f_input:
     changes = np.asarray([int(line) for line in f_input])
+    #SOLUTION TO PART 1
     print(np.sum(changes))
 
     i = 0
@@ -15,6 +18,7 @@ with open("./input") as f_input:
         frequencies = np.append(frequencies, [freq])
         freq += changes[i]
         if len(np.where(frequencies == freq)[0]) != 0:
+            #SOLUTION TO PART 2
             print(freq)
             flag = False
             break
