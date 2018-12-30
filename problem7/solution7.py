7673337e7edf9c4317d3d119f736884a94741e48
明俊 <daniel.simon.gonzalez.descamps@gmail.com>
2018-12-29 23:53:42 -0500

    Changes to be committed: 	modified:   solution7.py

--- problem7/solution7.py
+++ problem7/solution7.py
@@ -1,6 +1,21 @@
 from __future__ import print_function
 import numpy as np
 
+class Task:
+    def __init__(self, name):
+        self.name = name
+        self.time = ord(name) - 4
+    def __repr__(self):
+        return self.name
+    def __hash__(self):
+        return hash(self.name)
+    def __eq__(self, other):
+        return self.name == other.name
+    def __lt__(self, other):
+        return self.name < other.name
+    def __gt__(self, other):
+        return self.name > other.name
+
 def no_parent(v, E):
     return True if len({x for (x, y) in E if v == y}) == 0 else False
 
@@ -15,11 +30,9 @@ def top_sort(S, E):
         S.extend([x for (x, y) in e if no_parent(x, e)])
         S = sorted(set(S))
     T.extend(sorted([y for (x, y) in E]))
-    return T
-
-instr = [(line[5], line[36]) for line in open('./input.txt')]
-start = sorted({x for (x, y) in instr if no_parent(x, instr)})
+    return ''.join(T)
 
 #SOLUTION TO PART 1
-order = top_sort(start, instr)
-print(''.join(order))
+instr = [(line[5], line[36]) for line in open('./input.txt')]
+start = sorted({x for (x, y) in instr if no_parent(x, instr)})
+print(top_sort(start, instr))
